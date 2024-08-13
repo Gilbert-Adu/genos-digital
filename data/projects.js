cd14d8c2b7664f4d18af071a225cd1e107fe94bd
Gilbert Adu <gea33@cornell.edu>
2024-08-13 07:05:13 -0400

    new commit with ui changes

--- data/projects.js
+++ data/projects.js
@@ -1,6 +1,36 @@
 const projects = [
     {
         id: 1,
+        clientName: "Stripe eCommerce",
+        link: "https://ecomm-w-stripe.onrender.com/",
+        tags: "Stripe API, eCommerce, Content Management",
+        more: "This is a basic demo project showing the skeleton of an eCommerce website with Stripe payment API integration. Frontend is built with React JS and backend is built with Node JS.",
+        features: [
+            "Stripe API",
+            "Search",
+            "Content Management System",
+            "eCommerce"
+        ],
+        tools: [
+            "Node JS",
+            "Vanilla JS",
+            "GSAP",
+            "EJS",
+            "ReactJS",
+            "Express",
+            "Render", 
+            "CSS",
+            "dotenv",
+            "Git"
+
+
+        ],
+        videoSrc: "/images/stripevid.mov",
+        thumbnail: "/images/stripe-comm-2.png",
+    
+    },
+    {
+        id: 2,
         clientName: "Amhara Unity",
         link: "https://www.amharaunity.com/",
         tags: "Social media, Realtime chat, Blog CMS, Authentication",
