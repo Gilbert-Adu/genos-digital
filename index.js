8918c0545baff2fd8f1be301191db0ed2c99963b
Gilbert Adu <gea33@cornell.edu>
2025-01-20 02:15:56 -0500

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.get("/projects/:id", (req, res) => {
     const theProject = projects.find(project => project.id == req.params.id);
     res.render('project', {project: theProject})
 });
-app.listen(5000, () => {
+app.listen(80, () => {
     console.log("up on 80")
     console.log("I see these new changes.")
     
